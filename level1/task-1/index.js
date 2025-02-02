da138e331197d4640e26bd91eb832c1f10bbbce8
prachita789 <prachitagajbhiye97@gmail.com>
2025-02-02 23:48:28 +0530

    day4-level1-task2.3

--- level1/task-1/index.js
+++ level1/task-1/index.js
@@ -1,4 +1,4 @@
-
+//change the button color on click
 function colorChangeButton() {
     let button = document.querySelector(".color-btn");
     let colors = ["rgba(255, 94, 0, 0.95)", "rgb(255, 105, 180)", "rgb(40, 167, 69)", "rgb(255, 193, 7)", "rgb(111, 66, 193)"]; 
@@ -12,6 +12,7 @@ function colorChangeButton() {
 }
 
 
+// to display time-based greeting
 function getAlert(){
     const currentTime = new Date();
     const currentHour = currentTime.getHours();
@@ -39,20 +40,72 @@ function getAlert(){
     }, 5000);
 }
 
+
+
 window.onload = function() {
     setTimeout(getAlert, 3000);
 };
 
+
+//to calculate daily water intake based on weight
 function hydroCalculator(){
     const wt = document.getElementById("weight").value;
 
-    if(!wt){
+    if(!wt || wt <= 0){
         alert("Please enter your weight!");
         return;
     }
 
     const dailyWaterIntake = wt * 0.03;
-    
-
     document.getElementById("result").innerHTML = `You should drink <strong>${dailyWaterIntake.toFixed(1)} liters</strong> of water daily.`;
 }
+
+// function to count calorie intake
+function calorieCalculator() {
+
+    const breakfast = document.getElementById("breakfast").value.trim();
+    const lunch = document.getElementById("lunch").value.trim();
+    const dinner = document.getElementById("dinner").value.trim();
+    const snacks = document.getElementById("snacks").value.trim();
+
+    if(breakfast === "" && lunch === "" && dinner === "" && snacks === ""){
+        alert("Please enter at least one meal or snack!");
+        return;
+    }
+
+    if(isNaN(breakfast) || isNaN(lunch) || isNaN(dinner) || isNaN(snacks) ||
+breakfast < 0 || lunch < 0 || dinner < 0 || snacks < 0)
+{
+    alert("Please enter valid non-negative numbers for calories.");
+    return;
+}
+
+   breakfast = breakfast === "" ? 0 : Number(breakfast);
+   lunch = lunch === "" ? 0 : Number(lunch);
+   dinner = dinner === "" ? 0 : Number(dinner);
+   snacks = snacks === "" ? 0 : Number(snacks);
+
+   const totalCalories = breakfast + lunch + dinner + snacks;
+
+   document.getElementById("calorie-result").innerHTML = `Your total calorie intake today is <strong>${totalCalories} kcal</strong>.`;
+}
+
+//for switching tab
+function switchTab(type){
+    const sections = {
+       hydration: document.getElementById("hydration-calculator"),
+       calorie: document.getElementById("calorie-calculator")
+
+    };
+
+    const tabButton = document.querySelectorAll(".tab-btn");
+
+    for(let key in sections){
+        sections[key].style.display = "none";
+    }
+
+    tabButton.forEach(btn => btn.classList.remove("active"));
+
+    sections[type].style.display = "block";
+    document.querySelector(`.tab-btn[onclick="switchTab('${type}')"] `).classList.add("active");
+} 
